c43675803b48d0f344fd7d086d7c767fac2878f4
Umer Farooq <k142229@nu.edu.pk>
2017-08-09 19:09:02 +0500

    Create 11.R

---
+++ 11.R
@@ -0,0 +1,42 @@
+ print("hello")
+[1] "hello"
+> print(2+3)
+[1] 5
+
+ mystring <- "umer"
+> print(mystring)
+[1] "umer"
+> v<- TRUE
+> print(class(v))
+[1] "logical"
+> v<-23.4
+> print(class(v))
+[1] "numeric"
+> v<-23
+> print(class(v))
+[1] "numeric"
+> x <- "karachi"
+> print(class(x))
+[1] "character"
+> apple<- c('red','blue','green')
+> print(apple)
+[1] "red"   "blue"  "green"
+> print(class(apple)
++ )
+[1] "character"
+> list1 <- list(c(2,5,3),21.3,sin)
+> print(list1)
+[[1]]
+[1] 2 5 3
+
+[[2]]
+[1] 21.3
+
+[[3]]
+function (x)  .Primitive("sin")
+
+> M = matrix( c('a','a','b','c','b','a'), nrow = 2, ncol = 3, byrow = TRUE)
+> print(M)
+     [,1] [,2] [,3]
+[1,] "a"  "a"  "b" 
+[2,] "c"  "b"  "a" 
