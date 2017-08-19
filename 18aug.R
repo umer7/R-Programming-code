29891ca1cf9b133d8dc42e895a9440157fd91709
Umer Farooq <k142229@nu.edu.pk>
2017-08-19 17:27:49 +0500

    Create 18aug.R

---
+++ 18aug.R
@@ -0,0 +1,88 @@
+library(readr)
+new <- read_csv("D:/data/new.xlsx")
+View(new)
+library(caret)
+library(caret);library(kernlab);data(spam)
+library(ISLR)
+library(ggplot2)
+library(ggplot2)
+library(ggplot)
+library(caret)
+data("Wage")
+summary(Wage)
+in train <-createDataPartition(y=Wage$wage,p=0.7,list=False)
+inTrain <-createDataPartition(y=Wage$wage,p=0.7,list=False)
+inTrain <-createDataPartition(y=Wage$wage,p=0.7,list=False)
+installed.packages("ggplot2")
+library(ggplot2)
+installed.packages("ggplot")
+library(ggplot2)
+install.packages("rlang")
+library(ggplot2)
+library(kernlab)
+library(kernlab);
+install.packages("splane")
+install.packages("splanes")
+library(splines)
+library(kernlab);
+library(ggplot2)
+library(ISLR);library(ggplot2); librar''
+library(ISLR);library(ggplot2); library(caret);
+data("Wage")
+summary(Wage)
+mydata11 = read.csv("D:/data/2016/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv")
+data("mydata11")
+summary(mydata11)
+inTrain <- createDataPartition(y=Wage$wage,p=0.7,list=FALSE)
+training <-Wage[inTrain,]
+testing <-Wage[-inTrain,]
+dim(training)
+dim(testing)
+featurePlot(x=training[,c("age","education","jobclass")],y=training$wage,plot="pairs")
+qplot(age,wage,data=training)
+t1<-table(cutWage,training$jobclass)
+t1 <- table(cutWage,training$jobclass)
+cutWage <- cut2 (training$wage,g=3)
+savehistory("~/18aug.Rhistory")
+mydata11 = read.csv("D:/data/2016/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv")
+summary(mydata11)
+training <-mydata11[inTrain,]
+testing <-mydata11[-inTrain,]
+dim(training)
+dim(testing)
+featurePlot(x=training[,c("Physician_Specialty")],y=training$mydata11,plot="pairs")
+library(ggplot2)
+featurePlot(x=training[,c("Physician_Specialty")],y=training$mydata11,plot="pairs")
+qplot(Physician_Specialty,mydata11,data=training)
+library(kernlab)
+library(kernlab);
+data(spam)
+spam$capitalAveSq <- spam$capitalAve^2
+head(mydata11)
+lm <- lm (Total_Amount_Invested_USDollars - Value_of_Interest ,data = mydata11)
+lm <- lm (Total_Amount_Invested_USDollars
+- Value_of_Interest ,data = mydata11)
+lm <- lm ('Total_Amount_Invested_USDollars'
+- Value_of_Interest ,data = mydata11)
+lm <- lm ('Total_Amount_Invested_USDollars'
+- 'Value_of_Interest' ,data = mydata11)
+lm <- lm ('Total_Amount_Invested_USDollars'
+- 'Value_of_Interest' ,data = mydata11)
+lm1 <- lm ('Total_Amount_Invested_USDollars'
+- 'Value_of_Interest' ,data = mydata11)
+library(caret)
+library(caret);
+lm1 <- lm ('Total_Amount_Invested_USDollars'
+- 'Value_of_Interest' ,data = mydata11)
+summary(lm1)
+lm1 <- lm ("Total_Amount_Invested_USDollars"
+- "Value_of_Interest" ,data = mydata11)
+mydata11 = read.csv("D:/data/2016/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv")
+training <-mydata11[inTrain,]
+testing <-mydata11[-inTrain,]
+lm1 <- lm ("Total_Amount_Invested_USDollars"
+- "Value_of_Interest" ,data = mydata11)
+summary(lm1)
+lm1 <- lm ("Value_of_Interest" -"Total_Amount_Invested_USDollars" ,data = mydata11)
+library(caret); data("faithful"); set.seed(333)
+savehistory("~/18aug.Rhistory")
